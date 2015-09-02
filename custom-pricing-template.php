f9b73d32a1a8bdc4c38eac31af6d4abc7b231c74
Tim van der Slik Laptop <timvdslik@gmail.com>
2015-09-02 11:30:57 +0200

    Pricing Template page update, Fontello icons update CSS bootstrap 7 cols

--- custom-pricing-template.php
+++ custom-pricing-template.php
@@ -10,12 +10,51 @@ get_header(); ?>
     $args = array( 'post_type' => 'pricing');
     $the_query = new WP_Query($args);
 
-    while($the_query->have_posts() ) : $the_query->the_post(); ?>
+    // Get the services from the pricing options
+    $options = get_option('tvds_theme_pricing_options');
+    $services = $options['pricing'];
+    $filtered = array_filter($services);
 
-        <div class="row">
-            <h2><?php the_title(); ?></h2>
-            <?php the_content(); ?>
-            <a href="<?php echo get_permalink() ?>">bekijk meer</a>
+    while($the_query->have_posts() ) : $the_query->the_post();
+        // Get the checked options
+        $checked = get_post_meta($post->ID, '_pricing_check', true);
+        ?>
+
+        <div class="row <?php echo strtolower(get_the_title());; ?>">
+            <div class="container">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1><?php the_title(); ?></h1>
+
+                    <?php the_content(); ?>
+                </div>
+
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="services-list-horizontal row seven-cols no-margin">
+                        <h2>Inhoud</h2>
+                        <?php
+                        foreach($filtered as $service){
+                            if(in_array($service, $checked)){
+                                ?>
+                                <div class="col-md-1 service-list-horizontal include"><i class="icon-<?php echo strtolower($service); ?>"></i><p><?php echo $service ?></p></div>
+                                <?php
+                            } else {
+                                ?>
+                                <div class="col-md-1 service-list-horizontal exclude"><i class="icon-<?php echo strtolower($service); ?>"></i><p><?php echo $service ?></p></div>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="price-amount">
+                        <h2>Prijs €<?php echo get_post_meta($post->ID, '_pricing_price', true); ?>,-</h2>
+                    </div>
+                    <a href="<?php echo the_permalink(); ?>" class="btn btn-primary buy">Bekijk details</a>
+                </div>
+
+            </div>
         </div>
 
     <?php endwhile; wp_reset_postdata(); ?>
